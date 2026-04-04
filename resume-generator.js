821a0a023700d9b7a9316600afea2adf7f38f78d
marunmeera <marunmeera85@gmail.com>
2026-04-04 17:24:07 +0530

    Refactor resume generator functions for clarity

--- resume-generator.js
+++ resume-generator.js
@@ -5,54 +5,48 @@ btn.parentElement.remove();
 function addAcademic(){
 const div = document.createElement("div");
 div.className = "block";
-
 div.innerHTML = `
 <button class="remove-btn" onclick="removeBlock(this)">-</button>
 <input placeholder="Qualification">
 <input placeholder="Institution">
 <input placeholder="Grade">
 `;
-
 document.getElementById("academics").prepend(div);
 }
 
 function addExperience(){
 const div = document.createElement("div");
 div.className = "block";
-
 div.innerHTML = `
 <button class="remove-btn" onclick="removeBlock(this)">-</button>
 <input placeholder="Company Name">
-<input placeholder="Designation">
+<input placeholder="Role">
 <input placeholder="Start Date">
 <input placeholder="End Date">
 <textarea placeholder="Responsibilities"></textarea>
 `;
-
 document.getElementById("experience").prepend(div);
 }
 
 function addSkill(){
 const div = document.createElement("div");
 div.className = "block";
-
 div.innerHTML = `
 <button class="remove-btn" onclick="removeBlock(this)">-</button>
 <input placeholder="Skill">
 `;
-
 document.getElementById("skillsSection").prepend(div);
 }
 
 function preparePayment(){
 
 const academics = [];
-document.querySelectorAll("#academics .block").forEach(b => {
-academics.push([...b.querySelectorAll("input")].map(i => i.value));
+document.querySelectorAll("#academics .block").forEach(b=>{
+academics.push([...b.querySelectorAll("input")].map(i=>i.value));
 });
 
 const experience = [];
-document.querySelectorAll("#experience .block").forEach(b => {
+document.querySelectorAll("#experience .block").forEach(b=>{
 experience.push({
 company: b.querySelectorAll("input")[0].value,
 role: b.querySelectorAll("input")[1].value,
@@ -63,7 +57,7 @@ desc: b.querySelector("textarea").value
 });
 
 const skills = [];
-document.querySelectorAll("#skillsSection .block").forEach(b => {
+document.querySelectorAll("#skillsSection .block").forEach(b=>{
 skills.push(b.querySelector("input").value);
 });
 
@@ -80,7 +74,7 @@ skills
 localStorage.setItem("resumeData", JSON.stringify(data));
 }
 
-window.onload = () => {
+window.onload = ()=>{
 addAcademic();
 addExperience();
 };
